74278b1597f62490867afda52c8d74a04d2e3558
DSNG <117538221+LIONHOOD@users.noreply.github.com>
2024-01-12 15:34:12 +0900

    [level Lv.2] Title: 소수 찾기, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/Lv.2/42839. 소수 찾기/소수 찾기.py
@@ -1,11 +1,3 @@
 def solution(numbers):
-    answer = 0
     from itertools import permutations as P
-    X = []
-    for l in range(1,len(numbers)+1):
-        for p in set(P(numbers,l)):
-            X.append(int(''.join(p)))
-    X = set(X)
-    for x in X:
-        answer += [0,1][all(x%d for d in range(2,int(x**0.5)+1)) and x>1]
-    return answer
+    return sum([0,1][all(x%d for d in range(2,int(x**0.5)+1)) and x>1] for x in {int(''.join(p)) for l in range(1,len(numbers)+1) for p in P(numbers,l)})
