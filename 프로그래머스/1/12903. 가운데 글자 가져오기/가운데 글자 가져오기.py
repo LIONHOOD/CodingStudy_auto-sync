10fdffad9266f8e5c091db89d3e7a5b1495dff2d
DSNG <117538221+LIONHOOD@users.noreply.github.com>
2025-04-06 22:36:42 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,7 +1,8 @@
 def solution(s):
     l = len(s)
-    if l%2==0:
-        answer = s[l//2-1:l//2+1]
-    else:
-        answer = s[l//2]
+    # if l%2==0:
+    #     answer = s[l//2-1:l//2+1] 2k-1 = 2(k-1)+1  k-1:k+1
+    # else:
+    #     answer = s[l//2] k:k+1
+    answer = s[(l-1)//2:l//2+1]
     return answer
